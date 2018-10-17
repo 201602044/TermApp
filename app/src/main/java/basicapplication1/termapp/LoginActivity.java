417fda6536c6f3b3d82cd4e8680476e08d6c0f74
sonyeji <c201602044@cnu.ac.kr>
2018-10-17 17:17:25 +0900

    connect login db and make auto login

--- app/src/main/java/basicapplication1/termapp/LoginActivity.java
+++ app/src/main/java/basicapplication1/termapp/LoginActivity.java
@@ -1,17 +1,86 @@
 package basicapplication1.termapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 /**
  * Created by sj on 2018-10-10. tesgt
  */
+
 public class LoginActivity extends AppCompatActivity {
     //test first
+
+    private CheckBox checkBox;
+    SharedPreferences pref;
+    private  EditText editText;
+   private  EditText editText2;
+    private  SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        checkBox=(CheckBox) findViewById(R.id.login_checkbox);
+        editText=((EditText) findViewById(R.id.login_id));
+        editText2=((EditText) findViewById(R.id.login_passwd));
+        pref=getSharedPreferences("pref",MODE_PRIVATE);
+       checkBox.setChecked(pref.getBoolean("login_checkbox",false));
+        if(checkBox.isChecked()) {
+            editText.setText(pref.getString("user_id",""));
+            editText2.setText(pref.getString("user_passwd",""));
+//            login(); 주석해제시 자동 로그인
+        }
+
     }
+   public  void setPreferences(String id, String passwd){
+     editor = pref.edit();
+       editor.putString("user_id",id);
+       editor.putString("user_passwd",passwd);
+       editor.putBoolean("login_checkbox",true);
+       editor.commit();
+   }
+    public  void onClick(View v){
+        switch (v.getId()){
+            case R.id.login_to_main:
+                login();
+                break;
+            case  R.id.login_to_signup:
+                Intent intent;
+                intent=new Intent(this,SignupActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+public void login(){
+    String id=editText.getText().toString();
+    String passwd=editText2.getText().toString();
+    try{
+        LoginTask task   =   new LoginTask();
+        String result=task.execute(id,passwd).get();
+        if(result.equals("1"))
+        {
+            Intent intent=new Intent(this,MainActivity.class);
+            intent.putExtra("user_id",id);
+            intent.putExtra("user_passwd",passwd);
+            if(checkBox.isChecked()) setPreferences(id,passwd);
+            startActivity(intent);
+            finish();
+        }
+        else    {
+            editText.setText("");
+            editText2.setText("");
+        }
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+}
+
 }
 
--- app/src/main/java/basicapplication1/termapp/SignupActivity.java
+++ app/src/main/java/basicapplication1/termapp/SignupActivity.java
@@ -1,12 +1,7 @@
 package basicapplication1.termapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by sj on 2018-10-10.
@@ -18,9 +13,10 @@ public class SignupActivity   extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
-    public void onClick(View v){
+
+
+   /* public void onClick(View v){
         EditText id=(EditText) findViewById(R.id.signup_id);
-        TextView set_id_toast=(TextView) findViewById(R.id.signup_toast_id);
         if(id.getText().length()==0) {
             Toast.makeText(getApplicationContext(),"아이디가 공백입니다",Toast.LENGTH_SHORT);
             return;
@@ -32,18 +28,17 @@ public class SignupActivity   extends Activity {
             Toast.makeText(getApplicationContext(),"비밀번호가 공백입니다",Toast.LENGTH_SHORT);
             return;
         }
-        else if(password.getText().equals(password2.getText())){
-            TextView set_id_password=(TextView) findViewById(R.id.signup_toast_password);
-            //비번중복처리
-            set_id_password.setText("불일치");
+        if(!password.getText().equals(password2.getText())){
+            Toast.makeText(getApplicationContext(),"비밀번호가 같지않습니다.",Toast.LENGTH_SHORT);
+            return;
         }
-       EditText nickname=(EditText) findViewById(R.id.signup_nickname);
+        EditText nickname=(EditText) findViewById(R.id.signup_nickname);
         //닉네임도 중복확인해줘야함
         EditText phonenumber=(EditText) findViewById(R.id.signup_phonenumber);
         EditText email=(EditText) findViewById(R.id.signup_mail);
         if(nickname.getText().length()==0 || phonenumber.getText().length()==0 || email.getText().length()==0){
             Toast.makeText(getApplicationContext(),"공백입니다.",Toast.LENGTH_SHORT);
-                return;
+            return;
         }
 
 
@@ -53,4 +48,5 @@ public class SignupActivity   extends Activity {
 
 
     }
+    */
 }
